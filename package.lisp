0dccabec15b5dc74da5e47619957646f8dda6094
Andy Peterson <andy.arvid@gmail.com>
2012-05-25 10:58:33 -0400

    removed arvid-utils dependency added file utils.lisp

--- package.lisp
+++ package.lisp
@@ -1,10 +1,12 @@
+
 ;;;; package.lisp
 
 (defpackage #:smacklisp
-  (:use #:cl #:alexandria #:arvid-utils)
+  (:use #:cl #:alexandria #:named-readtables)
   (:export smack
            fresh-smack
            smack-string
+           smack-string-values
            *smack-symbols*
             ))
 
