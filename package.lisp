622beba43b1e41179aa7a036ff2725c018ffd74c
aarvid <andy.arvid@gmail.com>
2013-09-25 14:23:08 -0400

    fixed package symbols

--- package.lisp
+++ package.lisp
@@ -1,13 +1,17 @@
 
 ;;;; package.lisp
+(in-package :cl-user)
 
 (defpackage #:smacklisp
   (:use #:cl #:alexandria #:named-readtables)
-  (:export smack
-           fresh-smack
-           smack-string
-           smack-string-values
-           *smack-symbols*
-           init-smack-interp
+  (:export #:smack
+           #:fresh-smack
+           #:smack-string
+           #:smack-string-values
+           #:*smack-symbols*
+           #:init-smack-interp
+           #:load-file
+           #:load-string
+           #:load-stream
             ))
 
